fb830aaade1765de904b713abeb36d4a2c926184
ronmeher <ronmeher@gmail.com>
2022-05-22 11:23:25 +0530

    Linked Program added

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _21MayTest
+{
+    internal class Node
+    {
+
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,24 +14,34 @@ namespace _21MayTest
 
         static void Main(string[] args)
         {
-            Console.WriteLine("21 May Test!");
+            //Console.WriteLine("21 May Test!");
 
-            PerfectNumbers f1 = new PerfectNumbers();
-            f1.Display();
+            //Console.WriteLine("Welcome to Perfect Numbers Program!");
+            //PerfectNumbers f1 = new PerfectNumbers();
+            //f1.Display();
 
+            //Console.WriteLine("Welcome to Reverse Numbers Program!");
             //ReverseNumbers f2 = new ReverseNumbers();
             //f2.Display();
 
+
+            //Console.WriteLine("Welcome to Generic Program!");
             //GenericsExample p3 = new GenericsExample();
-
-
             //int[] intArray = { 1, 2, 3, 4, 5 };
             //double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5 };
             //char[] charArray = { 'a', 'b', 'c', 'd' };
-
             //Program.Show(intArray);
             //Program.Show(doubleArray);
             //Program.Show(charArray);
+
+            //Console.WriteLine("Welcome to Linked List Program!");
+            //LinkedList l = new LinkedList();
+            //l.Add(70);
+            //l.Add(30);
+            //l.Add(56);
+            //l.Display();
+
+
         }
     }
     
